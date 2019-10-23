09dff7cbb064b840b0ef4d178209042b059d8771
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-23 05:22:59 +0300

    Add user model

---
+++ app/models.py
@@ -0,0 +1,7 @@
+class User(db.model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.string(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
