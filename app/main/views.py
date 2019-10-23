bb36fcc82b2d3b543edcfb233ef939038501d0fd
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-23 15:00:20 +0300

    Add route for adding a pitch

--- app/main/views.py
+++ app/main/views.py
@@ -12,5 +12,25 @@ def index():
     title = 'Home- Welcome to the Pitch Website'
     categories = PitchCategory.get_categories()
     return render_template('index.html', title=title, categories=categories)
+@main.route('/category/pitch/new/<int:id>', methods=['GET', 'POST'])
+@login_required
+def new_pitch(id):
+    '''
+    Function to check Pitches form
+    '''
+    form = PitchForm()
+    category = PitchCategory.query.filter_by(id=id).first()
+
+    if category is None:
+        abort(404)
+
+    if form.validate_on_submit():
+        actual_pitch = form.content.data
+        new_pitch = Pitches(actual_pitch=actual_pitch,
+                            user_id=current_user.id, category_id=category.id)
+        new_pitch.save_pitch()
+        return redirect(url_for('.category', id=category.id))
+
+    return render_template('new_pitch.html', pitch_form=form, category=category)
 
 
