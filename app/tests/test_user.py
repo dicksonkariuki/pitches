29b3b37a577a00c98effd3408cfb312c803b9f73
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-23 22:31:53 +0300

    Add appropriate unnittests

--- app/tests/test_user.py
+++ app/tests/test_user.py
@@ -1,9 +1,17 @@
 import unittest
 from app.models import User
 
-class UserModelTest(unittest.TestCase):
-    def setUp(self):
-        self.new_user = User(password ='dickson')
+class Usertest(unittest.TestCase):
 
-    def test_password_setter(self):
-        self.AssertTrue(self.new_user.pass_secure is not none)
+  def setUp(self):
+    self.new_user = User(password = 'admin')
+
+  def test_password_setter(self):
+    self.assertTrue(self.new_user.passW is not None)
+
+  def test_no_access(self):
+    with self.assertRaises(AttributeError):
+      self.new_user.password
+
+  def test_check_password(self):
+    self.assertTrue(self.new_user.check_password('admin'))
