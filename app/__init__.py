ae475d37bd742d4cd7dcb641e2d4bad6385b2b3e
Dickson Karis <dicksonkariuki4@gmail.com>
2019-10-23 10:47:01 +0300

    Add a pass-secure column

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 def create_app(config_name):
--- app/models.py
+++ app/models.py
@@ -3,6 +3,22 @@ class User(db.model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.string(255))
+    email = db.Column()
+    pass_secure = db.Column(db.String(255))
+        @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+
+    
+
 
     def __repr__(self):
         return f'User {self.username}'
